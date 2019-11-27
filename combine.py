db0ddd535ef5d0cb0abcbc67d8c3cd4922bd34d4
huangjk1103 <32481141+huangjk1103@users.noreply.github.com>
2019-11-27 18:46:05 +0800

    Update combine.py

--- combine.py
+++ combine.py
@@ -1,20 +1,29 @@
 #!/usr/bin/python
 #encoding = utf-8
 import pandas as pd
+import sys
 
-with open('/Users/mac/Desktop/浮霉状菌/E7_8797_G_annotation_promer-总体.csv') as file1:
-    csv_file = csv.DictReader(file1,delimiter = ',')
-    for i in csv_file:
-        l1 = []
-        l1.append(i['id'])
-        print (l1)
+def Usage():
+    print('usage:python *.py [csv_file1] [csv_file2])
+          
+def main():
+          
+    with open(sys.argv[1]) as file1:
+        csv_file = csv.DictReader(file1,delimiter = ',')
+        for i in csv_file:
+            l1 = []
+            l1.append(i['id'])
+            print (l1)
         
 
-with open('/Users/mac/Desktop/浮霉状菌/transcriptome/diffanalysis6db649/combineeeafd4/result.csv') as file2:
-    csv_file2 = csv.DictReader(file2,delimiter = ',')
-    for j in csv_file2:
-        l2 = []
-        l2.append(j['id'])
-
+    with open(sys.argv[2]) as file2:
+        csv_file2 = csv.DictReader(file2,delimiter = ',')
+        for j in csv_file2:
+            l2 = []
+            l2.append(j['id'])
+try:
+          main()
+except:
+          Usage()
 
     
